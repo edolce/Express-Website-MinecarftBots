ae7fbd297fc5566287eabbad520ca85c10d834e2
edolce <edoardoguani@gmail.com>
2021-09-13 09:33:03 +0200

    Initial commit

---
+++ public/javascripts/bots.js
@@ -0,0 +1,19 @@
+document.getElementById("console_input")
+    .addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.key === "Enter") {
+
+            let consoleText = document.getElementById("console_input").value
+            document.getElementById("console_input").value=''
+            consoleCommand(consoleText)
+
+        }
+    });
+
+function consoleCommand(consoleText){
+    var paragraph =document.getElementById('console')
+    var text = document.createTextNode('\n'+consoleText);
+    paragraph.appendChild(text);
+    var objDiv = document.getElementById("console");
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
